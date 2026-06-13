dbd41117b2ff70c3c8b5c556ee0c4a1e08729c95
Горбунова Дарья <damagorbunova@edu.hse.ru>
2026-06-13 22:57:28 +0000

    выполнено второе задание

--- run.sh
+++ run.sh
@@ -1,11 +1,17 @@
 case "$1" in
     build_generator)
-        docker build -t generator-image .
+        docker build -f Dockerfile -t generator-image .
         ;;
     run_generator)
         docker run --rm -v $(pwd)/data:/data generator-image
         ;;
     create_local_data)
         python generate.py ./local_data
+        ;;
+    build_reporter)
+        docker build -f Dockerfile.reporter -t reporter-image .
+        ;;
+    run_reporter)
+        docker run --rm -v $(pwd)/data:/data reporter-image
         ;;
 esac
